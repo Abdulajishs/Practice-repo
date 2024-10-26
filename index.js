289d46abbc974518051901db9aa340208ce5cded
Abdulajishs <abdulajeeshsb@gmail.com>
2024-10-26 17:55:24 +0530

    logs with addition

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ let b = [1,3,5];
 
 for (let index = 0; index < b.length; index++) {
     const element = b[index];
+    console.log(element+5)
 }
 let array = [1,2,3]
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
+    console.log(element+10)
     
 }
