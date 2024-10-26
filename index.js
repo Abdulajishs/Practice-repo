f0fafe7c5035681f479bf3b2310cb53d153f5dbb
Abdulajishs <abdulajeeshsb@gmail.com>
2024-10-26 17:52:00 +0530

    logs added

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ let b = [1,3,5];
 
 for (let index = 0; index < b.length; index++) {
     const element = b[index];
+    console.log(element)
 }
 let array = [1,2,3]
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
-    
+    console.log(element)
 }
